cec2f66394ef47b6e0c4c33db8d30b6b49bb42d5
Finn.Zhu <Promacanthus@gmail.com>
2025-08-05 16:10:57 +0800

    Create types.go

---
+++ pkg/tool/types.go
@@ -0,0 +1,45 @@
+package tool
+
+import (
+	"encoding/json"
+
+	deepseek "github.com/cohesion-org/deepseek-go"
+	"github.com/invopop/jsonschema"
+)
+
+// Definitions is the list of all tool definitions.
+var Definitions = []Definition{
+	ReadFileDefinition,
+	ListFilesDefinition,
+	EditFileDefinition,
+}
+
+// GenerateSchema generates a schema from a struct.
+func GenerateSchema[T any]() *deepseek.FunctionParameters {
+	reflector := jsonschema.Reflector{
+		AllowAdditionalProperties: false,
+		DoNotReference:            true,
+	}
+	var v T
+
+	schema := reflector.Reflect(&v)
+
+	properties := map[string]interface{}{}
+	for pair := schema.Properties.Oldest(); pair != nil; pair = pair.Next() {
+		properties[pair.Key] = pair.Value
+	}
+
+	return &deepseek.FunctionParameters{
+		Type:       "object",
+		Properties: properties,
+		Required:   schema.Required,
+	}
+}
+
+// Definition is the definition for a tool.
+type Definition struct {
+	Name        string                       `json:"name,omitempty"`
+	Description string                       `json:"description,omitempty"`
+	InputSchema *deepseek.FunctionParameters `json:"input_schema,omitempty"`
+	Function    func(input json.RawMessage) (string, error)
+}
